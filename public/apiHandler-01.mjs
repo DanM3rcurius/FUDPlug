66dcc919b3eb3115b6eb8c4905f095d09d7e4484
DanM3rcurius <t3chn0shaman@protonmail.com>
2024-01-25 16:15:30 +0100

    apiHandler.02- new exportApiREquests function

--- public/apiHandler-01.mjs
+++ public/apiHandler-01.mjs
@@ -50,30 +50,56 @@ export function processApiRequest(req, res) {
 
     try {
       const jsonBody = JSON.parse(body);
-      const content = jsonBody.content;
 
       // Check if the request method is GET or POST
       const method = req.method;
 
-      interactWithChatbot(content, method)
-        .then(apiResponse => {
-          // Use cors middleware
-          cors()(req, res, () => {
-            // Preflight request, respond with success
-            if (req.method === 'OPTIONS') {
-              res.writeHead(200);
-              res.end();
-              return;
-            }
+      if (method === 'GET') {
+        const { id, content } = jsonBody;
+        interactWithChatbot({ id, content }, method)
+          .then(apiResponse => {
+            // Use cors middleware
+            cors()(req, res, () => {
+              // Preflight request, respond with success
+              if (req.method === 'OPTIONS') {
+                res.writeHead(200);
+                res.end();
+                return;
+              }
 
-            res.end(JSON.stringify({ message: apiResponse }));
+              res.end(JSON.stringify({ message: apiResponse }));
+            });
+          })
+          .catch(error => {
+            console.error('Error processing API request:', error);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
           });
-        })
-        .catch(error => {
-          console.error('Error processing API request:', error);
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Internal Server Error');
-        });
+      } else if (method === 'POST') {
+        const { content } = jsonBody;
+        interactWithChatbot({ content }, method)
+          .then(apiResponse => {
+            // Use cors middleware
+            cors()(req, res, () => {
+              // Preflight request, respond with success
+              if (req.method === 'OPTIONS') {
+                res.writeHead(200);
+                res.end();
+                return;
+              }
+
+              res.end(JSON.stringify({ message: apiResponse }));
+            });
+          })
+          .catch(error => {
+            console.error('Error processing API request:', error);
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+          });
+      } else {
+        res.writeHead(405, { 'Content-Type': 'text/plain' });
+        res.end('Method Not Allowed');
+      }
     } catch (error) {
       console.error('Error parsing JSON:', error);
       res.writeHead(400, { 'Content-Type': 'text/plain' });
