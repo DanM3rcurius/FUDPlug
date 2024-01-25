475b740c03df91e3b04eb38f899e058df5176b48
DanM3rcurius <t3chn0shaman@protonmail.com>
2024-01-25 16:02:20 +0100

    apiHandler02 - trying to unf*ck my code

--- public/apiHandler-01.mjs
+++ public/apiHandler-01.mjs
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
-dotenv.config();
 import fetch from 'node-fetch';
 import cors from 'cors';
 
+dotenv.config();
 
 // Environment Variables
 const MAGICK_API_KEY = process.env.MAGICK_API_KEY;
@@ -10,40 +10,35 @@ const MAGICK_AGENT_ID = process.env.MAGICK_AGENT_ID;
 const MAGICK_API_URL = process.env.MAGICK_API_URL;
 
 export function interactWithChatbot(content, method = 'POST') {
-    let apiUrl = `${process.env.MAGICK_API_URL}/api`;  // Updated URL for both GET and POST requests
-  
-    const requestOptions = {
-      method: method,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': process.env.MAGICK_API_KEY,
-      }
-    };
-  
-    if (method === 'POST') {
-      // Include "content" and "id" in the request body for POST requests
-      const requestBody = {
-        agentId: process.env.MAGICK_AGENT_ID,
-        content: content
-      };
-  
-      requestOptions.body = JSON.stringify(requestBody);
+  let apiUrl = `${process.env.MAGICK_API_URL}/api`;
 
-  
-    return fetch(apiUrl, requestOptions)
-      .then(response => response.json())
-      .then(data => data.message)
-      .catch(error => {
-        console.error('Error interacting with chatbot:', error);
-        throw error;
-      });
-  }}
+  const requestOptions = {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': process.env.MAGICK_API_KEY,
+    },
+  };
+
+  if (method === 'POST') {
+    const requestBody = {
+      agentId: process.env.MAGICK_AGENT_ID,
+      content: content,
+    };
+
+    requestOptions.body = JSON.stringify(requestBody);
+  }
+
+  return fetch(apiUrl, requestOptions)
+    .then(response => response.json())
+    .then(data => data.message)
+    .catch(error => {
+      console.error('Error interacting with chatbot:', error);
+      throw error;
+    });
+}
 
 export function processApiRequest(req, res) {
-  const jsonBody = JSON.parse(body);
-  const content = jsonBody.content;
-  const id = jsonBody.id;
-  const apiKey = jsonBody.apiKey;
   let body = '';
 
   req.on('data', (chunk) => {
@@ -62,15 +57,17 @@ export function processApiRequest(req, res) {
 
       interactWithChatbot(content, method)
         .then(apiResponse => {
-      // Use cors middleware
-      cors()(req, res, () => {
-        // Preflight request, respond with success
-        if (req.method === 'OPTIONS') {
-          res.writeHead(200);
-          res.end();
-          return;
-        }
-          res.end(JSON.stringify({ message: apiResponse }));
+          // Use cors middleware
+          cors()(req, res, () => {
+            // Preflight request, respond with success
+            if (req.method === 'OPTIONS') {
+              res.writeHead(200);
+              res.end();
+              return;
+            }
+
+            res.end(JSON.stringify({ message: apiResponse }));
+          });
         })
         .catch(error => {
           console.error('Error processing API request:', error);
